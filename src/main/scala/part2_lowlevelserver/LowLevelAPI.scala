696453e23bf1a1d4bc1f2d5cb39bf330c62776d4
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-02 16:21:28 +0100

    the code to run the http code

--- src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
@@ -3,6 +3,7 @@ package part2_lowlevelserver
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse, StatusCodes}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 
@@ -29,6 +30,49 @@ object LowLevelAPI extends App {
       binding.terminate(2 seconds)
     case Failure(ex) => println(s"Server binding failed: $ex")
   }
+
+  /*
+    Method 1: synchronously serve HTTP response
+   */
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
+      HttpResponse(
+        StatusCodes.OK, // HTTP 200
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |Hello from Akka HTTP!
+            |</body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound, // 404
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |OOPS! Nasty, the result cannot be found
+            |</body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+  }
+
+  val httpSyncConnectionHandler = Sink.foreach[IncomingConnection] {connection =>
+    connection.handleWithSyncHandler(requestHandler)
+  }
+
+  // the following is a source and sink making akka stream which runs the http
+  Http().bind("localhost", 8000).runWith(httpSyncConnectionHandler)
 
 
 }
