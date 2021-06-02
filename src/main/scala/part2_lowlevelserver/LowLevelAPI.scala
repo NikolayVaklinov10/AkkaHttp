593e70117f0f786c0f074bf875633d16848ef0d9
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-02 15:55:37 +0100

    the connections made in akka http

--- src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
@@ -1,13 +1,34 @@
 package part2_lowlevelserver
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.IncomingConnection
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 object LowLevelAPI extends App {
 
   // the two implicits needed for akka streams on which akka http stands on top of
   implicit val system = ActorSystem("LowLevelServerAPI")
   implicit val materializer = ActorMaterializer()
+  // a system.dispatcher import for running a futures capabilities
+  import system.dispatcher
+
+  val serverSource = Http().bind("localhost", 8000)
+  val connectionSink = Sink.foreach[IncomingConnection] { connection =>
+    println(s"Accepted incoming connection from ${connection.remoteAddress}")
+  }
+
+  val serverBindingFuture = serverSource.to(connectionSink).run()
+  serverBindingFuture.onComplete {
+    case Success(binding) =>
+      println("Server binding successful")
+      binding.terminate(2 seconds)
+    case Failure(ex) => println(s"Server binding failed: $ex")
+  }
 
 
 }
