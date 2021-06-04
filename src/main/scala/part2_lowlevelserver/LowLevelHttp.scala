1773919275b0f256564d8b6cf2c354068d9c71c1
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-04 21:52:14 +0100

    the first part of the https code

--- src/main/scala/part2_lowlevelserver/LowLevelHttp.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelHttp.scala
@@ -1,10 +1,52 @@
 package part2_lowlevelserver
 
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
+
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.stream.ActorMaterializer
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 object LowLevelHttp extends App {
   implicit val system = ActorSystem("LowLevelHttps")
   implicit val materializer = ActorMaterializer()
+
+  val requestHolder: HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
+      HttpResponse(
+        StatusCodes.OK, // HTTP 200
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |   Hello from Akka HTTPS!
+            |</body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound, // 404
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |   OOPS! Sadly The resource can't be found.
+            |</body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+  }
+
+  val httpsBinding = Http().bindAndHandleSync(requestHandler, "localhost", 8443, HttpsContext.httpsConnectionContext)
+
 
 }
